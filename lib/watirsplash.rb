cff5e8c36a8276135df7a39297c7ddf5eebcf9f5
Jarmo Pertman <jarmo.p@gmail.com>
2010-07-28 05:57:57 +0300

    * renamed AutoItHelper

--- lib/watirsplash.rb
+++ lib/watirsplash.rb
@@ -7,7 +7,7 @@ require_rel "watirsplash/waiter"
 require_rel "watirsplash/spec_helper"
 require_rel "watirsplash/spec"
 require_rel "watirsplash/watir"
-require_rel "watirsplash/autoit"
+require_rel "watirsplash/autoit_helper"
 require_rel "watirsplash/util"
 WatirSplash::Util.load_environment
 
