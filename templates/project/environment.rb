de4edf37302777fcd5601fa297c4a5c5926d6237
Jarmo Pertman <jarmo.p@gmail.com>
2010-07-30 12:33:55 +0300

    fixed project template

--- templates/project/environment.rb
+++ templates/project/environment.rb
@@ -7,9 +7,8 @@
 # By default everything, which is not a spec file, will be loaded from spec directory
 # and it's subdirectories
 spec_dir = File.join(File.dirname(__FILE__), "spec/**/*.rb")
-filtered_spec_files = Dir.glob(spec_dir).delete_if do |file|
-  File.directory?(file) || File.basename(file) =~ /.*spec\.rb$/
+filtered_ruby_files = Dir.glob(spec_dir).delete_if do |file|
+  File.basename(file) =~ /.*_spec\.rb$/
 end
-require_all filtered_spec_files
+require_all filtered_ruby_files unless filtered_ruby_files.empty?
 require_rel "config.rb"
-require_rel "application_helper.rb"
