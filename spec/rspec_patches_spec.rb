bf689e50cc7350867af4c116de331757e69554b5
Jarmo Pertman <jarmo.p@gmail.com>
2011-07-07 20:38:24 +0300

    moved rspec matcher's Array#match_array spec to correct file

--- spec/rspec_patches_spec.rb
+++ spec/rspec_patches_spec.rb
@@ -5,6 +5,23 @@ describe "RSpec patches" do
   end
 
   context "RSpec::Matchers" do
+    context "Array#match_array" do
+      it "matches other arrays with regexps" do
+        expected_ary = ["1", "2", "3", /\d/]
+        ["1", "2", "3", "5"].should match_array(expected_ary)
+
+        expected_ary = ["1", ["2", /\d+/], /3/]
+        ["1", [], "4"].should_not match_array(expected_ary)
+        ["1", ["2", "55"], "3"].should match_array(expected_ary)
+      end
+
+      it "doesn't work with other objects except Array" do
+        lambda {"".should match_array("")}.should raise_exception
+        lambda {[].should match_array("")}.should raise_exception
+        lambda {"".should match_array([])}.should raise_exception
+      end
+    end
+
     context "#within" do
       it "can be used with #change" do
         t = Time.now
