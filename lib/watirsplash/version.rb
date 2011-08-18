fa43cd4a3742799cb6bc619a47cffcfd44983b23
Jarmo Pertman <jarmo.p@gmail.com>
2011-08-18 18:34:35 +0300

    bumped dependencies versions

--- lib/watirsplash/version.rb
+++ lib/watirsplash/version.rb
@@ -1,10 +1,10 @@
 module WatirSplash
   module Version
-    WATIRSPLASH = "2.1.0"
+    WATIRSPLASH = "2.1.1"
 
     # possible runtime dependencies
-    WATIR = "1.9.2"
-    WIN32SCREENSHOT = "~>1.0.4"
-    WATIR_WEBDRIVER = "~>0.2.5"
+    WATIR = "2.0.1"
+    WIN32SCREENSHOT = "~>1.0.5"
+    WATIR_WEBDRIVER = "~>0.3.1"
   end
 end
