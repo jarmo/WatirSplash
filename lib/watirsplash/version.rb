a336bf9d09e41009b9f083bf62e51d4928d12d4a
Jarmo Pertman <jarmo.p@gmail.com>
2011-10-07 09:39:18 +0300

    bump version change History.rdoc

--- lib/watirsplash/version.rb
+++ lib/watirsplash/version.rb
@@ -1,10 +1,10 @@
 module WatirSplash
   module Version
-    WATIRSPLASH = "2.4.0"
+    WATIRSPLASH = "2.4.1"
 
     # possible runtime dependencies
     WATIR = "2.0.2"
     WIN32SCREENSHOT = "~>1.0.5"
-    WATIR_WEBDRIVER = "~>0.3.4"
+    WATIR_WEBDRIVER = "~>0.3.5"
   end
 end
